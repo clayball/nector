44243dfc9d11686f2467dc270ccc9ce9e66e2ca4
ꓛ㇜ay <cwells73@gmail.com>
2017-04-13 14:48:18 -0400

    Update settings.py
    don't keep real secrets in public places ;)

--- nector/settings.py
+++ nector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')@4!dl==kge9^s*=cdaxc#bo^fo#tphd#6(qt0-+)%6r950868'
+SECRET_KEY = 'THISISTOPSECR3t,MAN!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
