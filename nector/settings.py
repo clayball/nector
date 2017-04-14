7ea9a4625d1d77500fe7b8a5210a96a0ac80d1f8
ꓛ㇜ay <cwells@cwells.org>
2017-04-13 23:34:40 -0400

    broke CSS into static/main.css, added white text in body.

--- nector/settings.py
+++ nector/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'nector_home', 'static'),
+)
