9dcc7f3d0b5300b19ca4279df01bcce7b100fe69
Brian Jopling <brj33@drexel.edu>
2017-04-19 16:56:35 -0400

    Fix no vulnerabilities bug in views.py

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -12,11 +12,13 @@ def index(request):
     vuln_list = Vulnerability.objects.all()
     host_list = []
     subnet_list = []
-    for vuln in vuln_list:
-        vuln_ip = vuln.ipv4_address
-        host_list.append(Host.objects.get(ipv4_address=vuln_ip))
-        subnet_address = vuln_ip.rsplit('.', 1)
-        subnet_list.append(Subnet.objects.get(ipv4_address__startswith=subnet_address[0]))
-
+    try:
+        for vuln in vuln_list:
+            vuln_ip = vuln.ipv4_address
+            host_list.append(Host.objects.get(ipv4_address=vuln_ip))
+            subnet_address = vuln_ip.rsplit('.', 1)
+            subnet_list.append(Subnet.objects.get(ipv4_address__startswith=subnet_address[0]))
+    except:
+        pass
     context = {'vuln_list' : vuln_list, 'host_list' : host_list, 'subnet_list' : subnet_list, 'host_data' : zip(vuln_list, host_list, subnet_list)}
     return render(request, 'vulnerabilities/vulnz.html', context)
