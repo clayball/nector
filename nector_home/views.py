236bf41687bed908b5866a3c1f9acd39fc7778bc
Brian Jopling <brj33@drexel.edu>
2017-04-24 13:20:24 -0400

    Add Detection and OSINT landing pages

--- nector_home/views.py
+++ nector_home/views.py
@@ -5,3 +5,9 @@ from django.template import loader
 
 def index(request):
     return render(request, 'nector_home/index.html')
+
+def osint(request):
+    return render(request, 'nector_home/osint.html')
+
+def detection(request):
+    return render(request, 'nector_home/detection.html')
