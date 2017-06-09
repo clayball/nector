1b7c5ce275ab96edd8e11f5d0bc79cb551e71e0d
Brian Jopling <brj33@drexel.edu>
2017-06-09 16:22:57 -0400

    Update Scans implementation.

--- scans/views.py
+++ scans/views.py
@@ -11,9 +11,14 @@ from django.template.context_processors import csrf
 from forms import ScansForm
 
 from hosts.models import Host
+from hosts.models import Subnet
 from django.db import models
 
 import json
+
+# Exporting CSV
+import csv
+from django.http import StreamingHttpResponse
 
 def index(request):
     '''
@@ -26,22 +31,24 @@ def index(request):
         form = ScansForm(request.POST)
         if form.is_valid():
             context = {}
-            processed_query = process_query(form)
-            context['host_list'] = processed_query
-            context['open_ports'] = get_open_ports(processed_query)
-            context.update(csrf(request))
-            context['form'] = ScansForm()
             context['checks'] = request.POST.getlist('checks')
+            context['rad'] = request.POST.getlist('rad')
+            processed_query = process_query(form, context['checks'], context['rad'])
+            context['open_ports'], context['host_list'] = get_open_ports_and_new_list(processed_query)
+            context['subnet_list'] = get_subnet_list(context['host_list'])
+            context['host_data'] = zip(context['host_list'], context['subnet_list'])
+            context.update(csrf(request))
+            context['form'] = form
             return render_to_response('scans/scans.html', context)
     context = {}
     context.update(csrf(request))
     context['form'] = ScansForm()
+    context['checks'] = ['ipv4_address', 'host_name', 'ports']
     return render_to_response('scans/scans.html', context)
 
 
-def process_query(form):
+def process_query(form, checks, rad):
     '''
-    Helper function for index().
     Parses data from form and gets all objects that have that data.
     Returns QuerySet of Hosts.
     '''
@@ -54,51 +61,132 @@ def process_query(form):
     location = form.cleaned_data['location']
     tags = form.cleaned_data['tags']
     notes = form.cleaned_data['notes']
-    # Check multiple ports
-    # TODO
-    '''if ',' in ports:
+
+    host_list = []
+
+    # Check if multiple ports entered (ex 80, 443)
+    if ',' in ports:
         ports = ports.split(',')
-        print ports'''
-    host_list = Host.objects.filter(ipv4_address__icontains=ipv4_address,
-                                    host_name__icontains=host_name,
-                                    ports__icontains=ports,
-                                    os__icontains=os,
-                                    lsp__icontains=lsp,
-                                    host_groups__icontains=host_groups,
-                                    location__icontains=location,
-                                    tags__icontains=tags,
-                                    notes__icontains=notes)
-    '''
-    # Remove all closed ports from host-list:
-    for h in host_list:
-        if h.ports:
-            port_json = json.loads(h.ports)
-            all_ports = []
-            for p in port_json:
-                all_ports.append(p)
-                if p == ports:
-                    if 'closed' in port_json[ports]:
-                        print h.ipv4_address
-                        host_list = host_list.exclude(ipv4_address=h.ipv4_address)
-            # Port searched for was part of port json, not the port # itself.
-            if ports not in all_ports:
-                host_list = host_list.exclude(ipv4_address=h.ipv4_address)
-    '''
+        host_list = Host.objects.filter(ipv4_address__icontains=ipv4_address,
+                                        host_name__icontains=host_name,
+                                        os__icontains=os,
+                                        lsp__icontains=lsp,
+                                        host_groups__icontains=host_groups,
+                                        location__icontains=location,
+                                        tags__icontains=tags,
+                                        notes__icontains=notes)
+        # Filter each specified port, one at a time.
+        for p in ports:
+            p = p.strip()
+            host_list = host_list.filter(ports__icontains='"'+p+'"'+':')
+    elif ports.strip():
+        # Single port entered, so single filter needed:
+        host_list = Host.objects.filter(ipv4_address__icontains=ipv4_address,
+                                        host_name__icontains=host_name,
+                                        ports__icontains='"'+ports+'"'+':',
+                                        os__icontains=os,
+                                        lsp__icontains=lsp,
+                                        host_groups__icontains=host_groups,
+                                        location__icontains=location,
+                                        tags__icontains=tags,
+                                        notes__icontains=notes)
+    else:
+        # No port entered:
+        host_list = Host.objects.filter(ipv4_address__icontains=ipv4_address,
+                                        host_name__icontains=host_name,
+                                        os__icontains=os,
+                                        lsp__icontains=lsp,
+                                        host_groups__icontains=host_groups,
+                                        location__icontains=location,
+                                        tags__icontains=tags,
+                                        notes__icontains=notes)
+
+    # Did we select Online-Only or Offline-Only or All Hosts?
+    if 'online' in rad:
+        host_list = host_list.exclude(notes='Offline')
+    elif 'offline' in rad:
+        host_list = host_list.exclude(notes='Online')
+
     return host_list
 
 
-def get_open_ports(host_list):
-    all_ports = {}
+def get_open_ports_and_new_list(host_list):
+    '''
+    Returns a tuple following (open_ports, host_list).
+    open_ports is a dict following {ip: [ports]}.
+    host_list is a QuerySet.
+    Example return value:
+        {'123.45.67.89': ['80', '443']}, <Host QuerySet>
+    If a port is not open, then we won't add the port to the dict.
+    If a port is not open, then we will remove the respective Host
+    from the host_list since we don't want to display that Host.
+    '''
+    open_ports = {}
     for h in host_list:
-        if h.ports:
+        if h.ports: # If the host has ports:
             port_json = json.loads(h.ports)
             h_ports = []
+            has_open_port = False
             for p in port_json:
-                h_ports.append(str(p))
-            all_ports[h.ipv4_address] = h_ports
-    return all_ports
+                # Port is open, add to list .
+                if port_json[p][0] == 'open':
+                    h_ports.append(str(p))
+                    has_open_port = True
+                # Port is closed, remove Host from host_list.
+                elif port_json[p][0] == 'closed':
+                    host_list = host_list.exclude(ipv4_address=h.ipv4_address)
+                else:
+                    print 'Something went wrong! (scans/views.py -> get_open_ports_and_new_list())'
+            # Make sure it has an open port before we add it to the final dict.
+            # Otherwise, we are throwing in an empty list, and who needs that?
+            if has_open_port:
+                open_ports[str(h.ipv4_address)] = h_ports
+    return open_ports, host_list
 
+
+def get_subnet_list(host_list):
+    subnet_list = []
+    for h in host_list:
+        h_ip = h.ipv4_address
+        subnet_address = h_ip.rsplit('.', 1)
+        try:
+            subnet_list.append(Subnet.objects.get(ipv4_address__startswith=subnet_address[0]))
+        except:
+            print 'ERROR: Could not find subnet for ' + h_ip
+    return subnet_list
+
+
+class Echo(object):
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
 
 def export(request):
-    # todo
-    return render_to_response('scans/scans.html')
+    response = ''
+    if request.method == "POST":
+        ipv4_addresses = request.POST.getlist('scan-ipv4_address')
+        host_names = request.POST.getlist('scan-host_name')
+        open_ports = request.POST.getlist('scan-open_ports')
+        oses = request.POST.getlist('scan-os')
+        lsps = request.POST.getlist('scan-lsp')
+        host_groups = request.POST.getlist('scan-host_groups')
+        locations = request.POST.getlist('scan-location')
+        tags = request.POST.getlist('scan-tags')
+        notes = request.POST.getlist('scan-notes')
+        host_data = zip(ipv4_addresses, host_names, open_ports, oses, lsps,
+                        host_groups, locations, tags, notes)
+        output = []
+        #pseudo_buffer = Echo() #new
+        #writer = csv.writer(pseudo_buffer) #new
+        response = HttpResponse(content_type='text/csv')
+        writer = csv.writer(response)
+        writer.writerow(['ip', 'name', 'port', 'os', 'lsp', 'group', 'loc', 'tag', 'note'])
+        for ip, name, port, os, lsp, group, loc, tag, note in host_data:
+            output.append([ip, name, port, os, lsp, group, loc, tag, note])
+        #response = StreamingHttpResponse(writer.writerows(output), content_type="text/csv") #new
+        writer.writerows(output)
+        #response['Content-Disposition'] = 'attachment; filename="export.csv"' #new
+    return response
