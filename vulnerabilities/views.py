4fcd9d5dd29c2050438fff605e23f4497ae3fa07
Brian Jopling <brj33@drexel.edu>
2017-06-21 15:56:28 -0400

    Update documentation.

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -6,13 +6,22 @@ from .models import Vulnerability
 from hosts.models import Host
 from hosts.models import Subnet
 
-# Create your views here.
 
 def index(request):
+    '''Renders page containing all vulnerabilities in the database.'''
+
+    # Retrieve all vulnerabilities in db.
     vuln_list = Vulnerability.objects.all()
+
+    # Lists will be passed as context.
     host_list = []
     subnet_list = []
+
     try:
+        # Iterate through all vulnerabilities,
+        # figure out what host each one corresponds
+        # to, and get that host's subnet address
+        # for hyperlinking in the template.
         for vuln in vuln_list:
             vuln_ip = vuln.ipv4_address
             corresponding_host = Host.objects.get(ipv4_address=vuln_ip)
@@ -21,5 +30,10 @@ def index(request):
             subnet_list.append(Subnet.objects.get(ipv4_address__startswith=subnet_address[0]))
     except:
         pass
-    context = {'vuln_list' : vuln_list, 'host_list' : host_list, 'subnet_list' : subnet_list, 'host_data' : zip(vuln_list, host_list, subnet_list)}
+
+    # Pass context to rendered page.
+    context = {'vuln_list' : vuln_list, 'host_list' : host_list,
+               'subnet_list' : subnet_list,
+               'host_data' : zip(vuln_list, host_list, subnet_list)}
+    
     return render(request, 'vulnerabilities/vulnz.html', context)
