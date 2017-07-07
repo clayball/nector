e2666b02e39be607d41dbc3acdc1873f6825f1a3
Brian Jopling <brj33@drexel.edu>
2017-07-07 13:13:26 -0400

    Reinstate password guidelines, display message upon error during registration.

--- nector/settings.py
+++ nector/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
-'''
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-'''
+
 
 
 # Internationalization
