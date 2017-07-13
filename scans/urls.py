1db061a8cf6a2ce54f66adc382ab14d8d5fa3670
Brian Jopling <brj33@drexel.edu>
2017-07-13 10:55:41 -0400

    Update urls

--- scans/urls.py
+++ scans/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^edit/$', views.edit_scan, name='edit_scan'),
-    url(r'^perform/$', views.perform_scan, name='perform_scan'),
     url(r'^delete/$', views.delete_scan, name='delete_scan'),
 ]
