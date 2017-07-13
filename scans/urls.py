2c99357ba1450d07e4c93a47b5cd2eef2b6bca04
Brian Jopling <brj33@drexel.edu>
2017-07-13 10:54:58 -0400

    Merge urls

--- scans/urls.py
+++ scans/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^edit/$', views.edit_scan, name='edit_scan'),
+    url(r'^perform/$', views.perform_scan, name='perform_scan'),
     url(r'^delete/$', views.delete_scan, name='delete_scan'),
 ]
