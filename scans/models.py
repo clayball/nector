dfa462b68420e752f92dfe3526a48f05fe6e1c55
Brian Jopling <brj33@drexel.edu>
2017-07-13 10:54:06 -0400

    Merge models

--- scans/models.py
+++ scans/models.py
@@ -1,14 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class ScanType(models.Model):
     """Model for types of nmap scans."""
-    scan_name = models.CharField(default='My Custom Scan', max_length=255)
+
+    user = models.ForeignKey(User, default=0)
+
+    scan_name = models.CharField(max_length=255, unique=True)
 
     # max_length = 19 in the event a range is used; 000.000.000.000-000
-    host_address = models.CharField(default='0.0.0.0', max_length=19)
+    host_address = models.CharField(max_length=19)
 
     ports = models.CharField(max_length=255)
 
@@ -16,4 +19,4 @@ class ScanType(models.Model):
     scan_options = models.CharField(max_length=255)
 
     def __str__(self):
-        return "%s, %s, %s" % (self.scan_name, self.host_address, self.ports)
+        return "%s, %s, %s, %s" % (self.scan_name, self.user, self.host_address, self.ports)
