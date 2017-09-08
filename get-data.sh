95af990b92e3e91b56d61d0e0a5e35b5255ca2f2
Brian Jopling <brj33@drexel.edu>
2017-09-08 13:18:28 -0400

    Adding master script that calls others.

---
+++ get-data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# get-data.sh by Brian Jopling, 2017
+# ----------------------------------
+# Checks that subnets.txt has been populated by user.
+# If so, calls the scripts `get-hosts.sh` and `get-hops.sh`.
+# get-hosts.sh will create hosts.xml and openports.xml.
+# get-hops.sh will create hops.csv.
+
+
+if [[ -s subnets.txt ]]
+then
+    echo '[*] subnets.txt found and is populated.';
+    ./get-hosts.sh;
+    ./get-hops.sh;
+else
+    echo '[!] File subnets.txt not found or is empty!';
+    echo '[!] Creating subnets.txt and prompting user...';
+    touch subnets.txt;
+    echo '0.0.0.0/24' > subnets.txt;
+    vi subnets.txt;
+fi
